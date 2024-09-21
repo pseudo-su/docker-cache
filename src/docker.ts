2158598807a72aae3aad19143247e2c7e9567ad6
John Stableford <john.stableford@gmail.com>
2024-09-21 23:49:25 +1000

    update

--- src/docker.ts
+++ src/docker.ts
@@ -65,7 +65,7 @@ const saveDockerImages = async (): Promise<void> => {
           "will be saved.",
       );
       const newImagesArgs = newImages.join(" ");
-      const cmd = `docker save --output ${DOCKER_IMAGES_PATH} ${newImagesArgs}`;
+      const cmd = `docker save ${newImagesArgs} | pigz --fast > ${DOCKER_IMAGES_PATH}`;
       await execBashCommand(cmd);
       await saveCache([DOCKER_IMAGES_PATH], key);
     }
